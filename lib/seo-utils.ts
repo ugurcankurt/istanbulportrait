cc5e32712d2491e0399fbc91a6dd5414ebfe1b81
Uğur Cankurt <ugurgraphic@gmail.com>
2026-04-13 06:41:39 +0300

    fix(seo): prioritize VERCEL_URL logic in getBaseUrl

--- lib/seo-utils.ts
+++ lib/seo-utils.ts
@@ -3,9 +3,9 @@ import { SiteSettings } from "./settings-service";
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
   if (process.env.VERCEL_PROJECT_PRODUCTION_URL) return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  if (process.env.NEXT_PUBLIC_APP_URL) return process.env.NEXT_PUBLIC_APP_URL;
   return "https://istanbulphotosession.vercel.app";
 }
 
