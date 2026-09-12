8b07be24d7386784cbc39f3577ef21fe5e33e3d0
Uğur Cankurt <ugurgraphic@gmail.com>
2026-09-12 19:09:27 +0300

    fix(seo): update robots.txt to point to new sitemap-index.xml

--- app/robots.ts
+++ app/robots.ts
@@ -25,6 +25,6 @@ export default function robots(): MetadataRoute.Robots {
         ...checkoutDisallows,
       ],
     },
-    sitemap: `${getBaseUrl()}/sitemap.xml`,
+    sitemap: `${getBaseUrl()}/sitemap-index.xml`,
   };
 }
